8434b492d7803c464f94dd94193f5fe5670b36b7
Chris Krycho <chris@chriskrycho.com>
2016-04-17 15:45:17 -0400

    Generate site with Pelican.

---
+++ pelicanconf.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Chris Krycho'
+SITENAME = 'Run With Me: A Micro-Podcast About Running'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+DEFAULT_DATE_FORMAT = "%B %d, %Y"
+DEFAULT_LANG = 'en'
+
+THEME = 'podcast_theme'
+
+THEME_STATIC_DIR = ''
+
+# No categories or tags
+DIRECT_TEMPLATES = ['index', 'archives']
+PAGINATED_DIRECT_TEMPLATES = ['archives']
+
+ARTICLE_SAVE_AS = 'runs/{number}/index.html'
+ARTICLE_URL = 'runs/{number}/'
+
+ARCHIVES_SAVE_AS = 'runs/index.html'
+
+# Disable unused pages
+CATEGORY_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+TAG_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+
+
+# All feeds for this particular site are disabled; I generate the feeds via
+# [Feeder] -- at least until such a time I create a podcast plugin for Pelican.
+# (Frankly, that might be never.)
+#
+# [Feeder]: http://reinventedsoftware.com/feeder/
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 12  # roughly 3 months. Ish.
+
+STATIC_PATHS = ['extra']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},
+                       'extra/favicon.ico': {'path': 'favicon.ico'},
+                       'extra/feed.xml': {'path': 'feed.xml'},
+                       'extra/.nojekyll': {'path': '.nojekyll'}}
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
